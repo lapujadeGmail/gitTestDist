cf4f76ed136e0282f3bd3392e24da9a6927248b1
Anne Lapujade <anne.lapujade@u-picardie.fr>
2023-10-17 15:28:30 +0200

    Deuxième commit de Models

--- Models/Utilisateur.cs
+++ Models/Utilisateur.cs
@@ -5,11 +5,13 @@ namespace ProjetGestionTaches.Models
 {
     class Utilisateur
     {
+        // j'ajoute des modifs pour tester git
         public int ID { get; set; }
         [Required]
         public String UserName { get; set; }
         public String Nom { get; set; }
         public String Prenom { get; set; }
+        public string Adresse { get; set; }
 
         public override string ToString()
         {
